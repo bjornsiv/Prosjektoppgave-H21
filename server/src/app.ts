c6cc29c6aede552834ac54548c0417f328a04745
Sivert <sivertbjorn@gmail.com>
2021-11-02 17:43:09 +0100

    Fixed typescript issue that preventet the server from booting

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import router from './db-router'
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/v2', router);
 
 export default app;
---
+++ server/src/mysql-pool.ts
@@ -0,0 +1,12 @@
+import mysql from 'mysql';
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  connectionLimit: 1,
+  typeCast: (field, next) =>
+    field.type == 'TINY' && field.length == 1 ? field.string() == '1' : next(),
+});
+
+export default pool;

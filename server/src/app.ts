bb3807b36cd67285686aae82b7bf79c46379a069
aidaran <aida.angell94@gmail.com>
2021-11-08 22:47:51 +0100

    Add restful server implementation

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import router from './db-router'
+import reviews from './review-router';
+import games from './game-router';
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/v2', router);
+app.use('/api/v1/reviews', reviews);
+app.use('/api/v1/games', games); 
 
 export default app;
+
+
+//app.ts hoster server

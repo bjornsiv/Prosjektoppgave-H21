b8ceeb8055cb39452ac81477121825964b63071e
Sivert <sivertbjorn@gmail.com>
2021-11-11 15:57:44 +0100

    added a single line in readme

---
+++ server/test/config.ts
@@ -0,0 +1,11 @@
+// DO NOT commit this file into version control
+
+// Make sure this file is added to .gitignore
+
+process.env.MYSQL_HOST = 'mysql.stud.ntnu.no';
+
+process.env.MYSQL_USER = 'sabjorns_todo';
+
+process.env.MYSQL_PASSWORD = 'PasswordIsAThingYouShouldKeepSecretJuleNissen';
+
+process.env.MYSQL_DATABASE = 'sabjorns_prosjekt';
